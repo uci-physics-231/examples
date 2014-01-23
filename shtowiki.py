d1af67481bedc75c1759448ebd4d67c87be5d74b
David Kirkby <dkirkby@uci.edu>
2014-01-23 08:36:14 -0800

    Use tempfile to get a clean workdir each time, and shutils to delete it

--- shtowiki.py
+++ shtowiki.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
+import tempfile
+import shutil
 import subprocess
 import os
 import os.path
@@ -9,10 +11,10 @@ import sys
 def main():
 	parser = argparse.ArgumentParser(description='Convert bash script into wiki markup')
 	parser.add_argument('-i','--input',default='',help='input script to process')
-	parser.add_argument('--tmp-path',default='shtowiki',metavar='NAME',help='run from /tmp/NAME')
 	parser.add_argument('--prompt',default='% ',help='command prompt to display')
 	args = parser.parse_args()
 
+	# Check input arg and prepare output name
 	if not args.input:
 		print 'Missing required input name.'
 		sys.exit(-1)
@@ -23,43 +25,50 @@ def main():
 	outname += '.textile'
 	print 'Generating %r' % outname
 
-	# Open files (before we change directories)
-	with open(args.input,'r') as input:
-		with open(outname,'w') as output:	
-			# Go to the requested directory, creating it if necessary.
-			where = os.path.join('/tmp',args.tmp_path)
-			if not os.path.exists(where):
-				os.mkdir(where)
-			os.chdir(where)
-			# Loop over lines in the input file.
-			last = None
-			for line in input:
-				if line.strip() == '':
-					last = None
-				elif line[0] == '#':
-					# Strip the comment sign from explanatory text and format as a paragraph.
-					if not last is 'text':
-						print >>output, '\np.',
-					print >>output, line[1:].strip()
-					last = 'text'
-				else:
-					# Format commands as block code.
-					cmd = line.rstrip()
-					if not last is 'cmd':
-						print >>output, '\npre.',
-					print >>output, args.prompt + cmd
-					# Try running the command.
-					try:
-						result = subprocess.check_output(cmd,shell=True)
-						if result:
-							# Display the output in a separate code block, possibly including blank lines.
-							print >>output, '\nbc.. ' + result
-							last = 'result'
-						else:
-							last = 'cmd'
-					except subprocess.CalledProcessError,e:
-						print 'Command failed: %r' % cmd
-						sys.exit(-3)
+	# Prepare the temporary working directory we will use
+	workdir = tempfile.mkdtemp()
+	print 'Working in %r' % workdir
+
+	# Wrap everything with a try/finally block to make sure we delete the workdir
+	try:
+		# Open files (before we change directories)
+		with open(args.input,'r') as input:
+			with open(outname,'w') as output:	
+				# Go to the working dir
+				os.chdir(workdir)
+				# Loop over lines in the input file.
+				last = None
+				for line in input:
+					if line.strip() == '':
+						last = None
+					elif line[0] == '#':
+						# Strip the comment sign from explanatory text and format as a paragraph.
+						if not last is 'text':
+							print >>output, '\np.',
+						print >>output, line[1:].strip()
+						last = 'text'
+					else:
+						# Format commands as block code.
+						cmd = line.rstrip()
+						if not last is 'cmd':
+							print >>output, '\npre.',
+						print >>output, args.prompt + cmd
+						# Try running the command.
+						try:
+							result = subprocess.check_output(cmd,shell=True)
+							if result:
+								# Display the output in a separate code block,
+								# possibly including blank lines.
+								print >>output, '\nbc.. ' + result
+								last = 'result'
+							else:
+								last = 'cmd'
+						except subprocess.CalledProcessError,e:
+							print 'Command failed: %r' % cmd
+							sys.exit(-3)
+	finally:
+		# recursively delete our workdir and its contents
+		shutil.rmtree(workdir)
 
 if __name__ == '__main__':
 	main()
