a67caac0fe44bd5f0df386860e3091cfc8f4b718
David Kirkby <dkirkby@uci.edu>
2014-01-23 13:55:46 -0800

    Add support for quiet commands, enclosed in parentheses

--- shtowiki.py
+++ shtowiki.py
@@ -38,34 +38,45 @@ def main():
 				os.chdir(workdir)
 				# Loop over lines in the input file.
 				last = None
+				textBuffer = ''
 				for line in input:
-					if line.strip() == '':
-						last = None
-					elif line[0] == '#':
-						# Strip the comment sign from explanatory text and format as a paragraph.
-						if not last is 'text':
-							print >>output, '\np.',
-						print >>output, line[1:].strip()
+					if line[0] == '#':
+						# Strip off the comment symbol and add to our text buffer
+						textBuffer += line[1:].strip() + ' '
 						last = 'text'
 					else:
-						# Format commands as block code.
-						cmd = line.rstrip()
-						if not last is 'cmd':
-							print >>output, '\npre.',
-						print >>output, args.prompt + cmd
-						# Try running the command.
-						try:
-							result = subprocess.check_output(cmd,shell=True,stderr=subprocess.STDOUT)
-							if result:
-								# Display the output in a separate code block,
-								# possibly including blank lines.
-								print >>output, '\nbc.. ' + result
-								last = 'result'
+						# Output anything in the text buffer as a paragraph.
+						if textBuffer:
+							print >>output, '\np.',textBuffer
+						textBuffer = ''
+						if line.strip() == '':
+							# Skip blank lines
+							last = 'blank'
+						else:
+							# Otherwise, this is a command
+							cmd = line.rstrip()
+							# We suppress the output from commands in parentheses
+							if cmd[0] == '(' and cmd[-1] == ')':
+								quiet = True
+								cmd = cmd[1:-1]
 							else:
-								last = 'cmd'
-						except subprocess.CalledProcessError,e:
-							print 'Command failed: %r' % cmd
-							sys.exit(-3)
+								quiet = False
+							# Keep consecutive commands with no output in the same code block
+							if not last is 'cmd':
+								print >>output, '\npre.',
+							print >>output, args.prompt + cmd
+							last = 'cmd'
+							# Try running the command.
+							try:
+								result = subprocess.check_output(cmd,shell=True,stderr=subprocess.STDOUT)
+								if result and not quiet:
+									# Display the output in a separate code block,
+									# possibly including blank lines.
+									print >>output, '\nbc.. ' + result
+									last = 'result'
+							except subprocess.CalledProcessError,e:
+								print 'Command failed: %r' % cmd
+								sys.exit(-3)
 	finally:
 		# recursively delete our workdir and its contents
 		shutil.rmtree(workdir)
