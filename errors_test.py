1cc2be7640cdfbd7aeeb1007f52a6770c5e4e6c3
David Kirkby <dkirkby@uci.edu>
2014-02-13 13:35:24 -0800

    Prefix test methods with test_

--- errors_test.py
+++ errors_test.py
@@ -13,18 +13,18 @@ class ValueWithErrorsTests(unittest.TestCase):
 	def setUp(self):
 		pass
 
-	def ctor_two_args(self):
+	def test_ctor_two_args(self):
 		"ctor requires exactly two args"
 		self.assertRaises(TypeError,errors.ValueWithError)
 		self.assertRaises(TypeError,errors.ValueWithError,1)
 		self.assertRaises(TypeError,errors.ValueWithError,1,2,3)
 
-	def ctor_neg_error(self):
+	def test_ctor_neg_error(self):
 		"error value must be positive"
 		self.assertRaises(ValueError,errors.ValueWithError,1,0)
 		self.assertRaises(ValueError,errors.ValueWithError,1,-1)
 
-	def str_rounding_range1(self):
+	def test_str_rounding_range1(self):
 		"rounding test cases with sig = 100 - 354"
 		self.assertEqual(str(errors.ValueWithError(1,1)),"1.00 +/- 1.00")
 		self.assertEqual(str(errors.ValueWithError(1,0.1)),"1.000 +/- 0.100")
@@ -34,9 +34,9 @@ class ValueWithErrorsTests(unittest.TestCase):
 		self.assertEqual(str(errors.ValueWithError(1,0.0354)),"1.0000 +/- 0.0354")
 		self.assertEqual(str(errors.ValueWithError(1,3.54999)),"1.00 +/- 3.54")
 		self.assertEqual(str(errors.ValueWithError(1,0.354999)),"1.000 +/- 0.354")
-		self.assertEqual(str(errors.ValueWithError(1,0.0354999)),"1.0000 +/- 0.0354")
+		self.assertEqual(str(errors.ValueWithError(1,0.0354999)),"1.000 +/- 0.0354")
 
-	def str_rounding_range2(self):
+	def test_str_rounding_range2(self):
 		"rounding test cases with sig = 355 - 949"
 		pass
 
