2072682dff02e66a4b285446114e4f6f3e74f146
David Kirkby <dkirkby@uci.edu>
2014-02-25 09:24:27 -0800

    Add example of factory method pattern in python

---
+++ patterns/factory.py
@@ -0,0 +1,30 @@
+# A sample implementation of the Factory Method design pattern in python
+# Test using:
+"""
+import factory as particle
+e = particle.create("electron")
+e.mass
+p = particle.create("proton")
+p.mass
+"""
+
+class Particle(object):
+	def __init__(self,mass):
+		self.mass = mass
+
+def create(name):
+	# define the subclasses of Particle that we can create as nested
+	# classes so that they cannot be created directly.
+	class Electron(Particle):
+		def __init__(self):
+			Particle.__init__(self,0.511)
+	class Proton(Particle):
+		def __init__(self):
+			Particle.__init__(self,938.)
+	# create the requested class by name
+	if name == "electron":
+		return Electron()
+	elif name == "proton":
+		return Proton()
+	else:
+		raise RuntimeError("Unknown particle name: %s",name)
